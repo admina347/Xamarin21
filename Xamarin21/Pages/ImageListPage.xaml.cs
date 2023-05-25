0516e820cd9b51e2d04ce55e425f8820b1fbc493
admina347 <it-mactep@mail.ru>
2023-05-25 17:46:32 +0500

    image show, image delete

--- Xamarin21/Pages/ImageListPage.xaml.cs
+++ Xamarin21/Pages/ImageListPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using CoreImage;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Shapes;
 using Xamarin.Forms.Xaml;
 using Xamarin21.Models;
 
@@ -13,11 +17,65 @@ namespace Xamarin21.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ImageListPage : ContentPage
     {
-        public List<GalleryImage> Images { get; set; } = new List<GalleryImage>();
+        GalleryImage SelectedImage;
+        public ObservableCollection<GalleryImage> Images { get; set; } = new ObservableCollection<GalleryImage>();
         public ImageListPage()
         {
             InitializeComponent();
+
+
+            string imagesPath = "/storage/emulated/0/DCIM/Camera";
+
+            try
+            {
+                IEnumerable<string> allImages = Directory.EnumerateFiles(imagesPath);
+                //DisplayAlert("Строк" + gridRows, "Найдено файлов" + allImages.Count(), "OK");
+                foreach (string filename in allImages)
+                {
+                    //Create an Image object for each bitmap
+                    //DisplayAlert("File", filename, "OK");
+                    string imageName = System.IO.Path.GetFileName(filename);
+                    string imageDescription = File.GetCreationTimeUtc(filename).ToString("yyyy:MM:dd HH:mm:ss");
+                    //DateTime imageDate = GetMyImageTakenDate(filename);
+                    //Image image = new Image
+                    //{
+                    //    Source = ImageSource.FromFile(filename),   //FromUri(new Uri(filepath))
+                    //    HeightRequest = 100
+
+                    //};
+                    //
+                    Images.Add(new GalleryImage(Guid.NewGuid(), imageName, image: filename, description: imageDescription));
+
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            BindingContext = this;
         }
+
+        //static DateTime GetMyImageTakenDate(string filename)
+        //{
+        //    DateTime takenDate = DateTime.Today;
+
+        //    CGImageSource myImageSource;
+        //    myImageSource = CGImageSource.FromUrl(filename, null);
+        //    var ns = new NSDictionary();
+        //    var imageProperties = myImageSource.CopyProperties(ns, 0);
+
+        //    NSObject date = null;
+        //    var exifDictionary = imageProperties.ValueForKey(ImageIO.CGImageProperties.ExifDictionary);
+        //    if (exifDictionary != null)
+        //    {
+        //        date = exifDictionary.ValueForKey(ImageIO.CGImageProperties.ExifDateTimeOriginal);
+        //    }
+        //    takenDate = date != null ? DateTime.ParseExact(date.ToString(), "yyyy:MM:dd HH:mm:ss", null) : takenDate;
+        //    return takenDate;
+        //}
 
         /// <summary>
         /// Обработчик нажатия
@@ -36,9 +94,41 @@ namespace Xamarin21.Pages
         private void imageList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             // распаковка модели из объекта
-            var selectedImage = (GalleryImage)e.SelectedItem;
-            // уведомление
-            DisplayAlert("Выбор", $"Вы выбрали {selectedImage.Name}", "OK"); ; ;
+            SelectedImage = (GalleryImage)e.SelectedItem;
+        }
+
+        /// <summary>
+        /// Обработчик добавления нового устройства
+        /// </summary>
+        private async void OpenImage(object sender, EventArgs e)
+        {
+            // проверяем, выбрал ли пользователь устройство из списка
+            if (SelectedImage == null)
+            {
+                await DisplayAlert(null, $"Пожалуйста, выберит изображение!", "OK");
+                return;
+            }
+
+            // Переход на следующую страницу - страницу нового устройства (и помещение её в стек навигации)
+            await Navigation.PushModalAsync(new ImagePage("Просмотр", SelectedImage));
+        }
+
+        /// <summary>
+        /// Обработчик удаления устройства
+        /// </summary>
+        private async void DeleteImage(object sender, EventArgs e)
+        {
+            // Получаем и "распаковываем" выбранный элемент
+            var imageToRemove = imageList.SelectedItem as GalleryImage;
+            if (imageToRemove != null)
+            {
+                // Удаляем
+                Images.Remove(imageToRemove);
+                //
+                File.Delete(imageToRemove.Image);
+                // Уведомляем пользователя
+                await DisplayAlert(imageToRemove.Name, $"Изображение '{imageToRemove.Image}' удалено", "ОК");
+            }
         }
     }
 }
---
+++ Xamarin21/Pages/ImagePage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin21.Models;
+
+namespace Xamarin21.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImagePage : ContentPage
+    {
+        public static string PageName { get; set; }
+        public static string ImageName { get; set; }
+        public static string ImagePath { get; set; }
+        public static string ImageDescription { get; set; }
+ 
+        public GalleryImage GalleryImage { get; set; }
+
+        public ImagePage(string pageName, GalleryImage galleryImage = null)
+        {
+            PageName = pageName;
+
+            if (galleryImage != null)
+            {
+                GalleryImage = galleryImage;
+                ImageName = galleryImage.Name;
+                ImagePath = galleryImage.Image;
+                ImageDescription = galleryImage.Description;
+            }
+            else
+            {
+                galleryImage = new GalleryImage(Guid.NewGuid(), name: "test");
+            }
+            InitializeComponent();
+            OpenImage(ImagePath, ImageDescription);
+        }
+
+        public void OpenImage(string filename, string desc)
+        {
+
+            Image image = new Image
+            {
+                Source = ImageSource.FromFile(filename),   //FromUri(new Uri(filepath))
+                
+
+            };
+            
+            stackLayout.Children.Add(image);
+            stackLayout.Children.Add(new Label { Text = desc, HorizontalTextAlignment = TextAlignment.Center, TextColor = Color.White });
+
+        }
+
+    }
+}
